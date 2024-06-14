3578c9449c2f7cd039c447f97bfb81d09ac2a931
Thomas Seeley <104278845+iamseeley@users.noreply.github.com>
2024-06-14 09:37:27 -0400

    Delete fuchsiaQuelea directory

--- fuchsiaQuelea/fuchsiaQuelea.ts
+++
@@ -1,9 +0,0 @@
-import handler from "https://esm.town/v/maxm/staticChess";
-import { pwa } from "https://esm.town/v/pomdtr/pwa";
-
-export default pwa(handler, {
-  name: "Static Chess",
-  display: "standalone",
-  background_color: "#ffffff",
-  start_url: "/",
-});
